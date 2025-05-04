00b0e762f538a009ef3d96f348f683a409066152
dhafinrazaqa <dhafin.razaqa12@gmail.com>
2025-05-04 18:08:39 +0700

    Implement authentication check in Dashboard component and add loading state

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,28 @@
+"use client";
+
 import Navbar from "@/components/navbar";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Dashboard() {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/signin"); // Redirect if not authenticated
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return (
+      <div className="flex items-center justify-center min-h-screen p-4 bg-gray-100">
+        <p className="text-lg">Loading...</p>
+      </div>
+    );
+  }
+
   return (
     <>
       <Navbar />
